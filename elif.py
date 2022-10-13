fc03f548163a8692d0d6eb8b1873d83f3c1f4c53
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-10-13 23:06:16 +0530

    Else and If

---
+++ elif.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks : "))
+
+if marks > 90 and marks <= 100:
+    print("Your grade is A")
+
+elif marks > 80 and marks <= 90:
+    print("Your grade is B")
+
+elif marks > 70 and marks <= 80:
+    print("Your grade is C")
+
+elif marks > 0 and marks <=70:
+    print("Your grade is D")
+else: print("Not Valid")
