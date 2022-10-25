0366cfe54b0358ddd6f6eee876b8b66368f05f12
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-10-25 22:19:40 +0530

    Square(If-else)

--- ifelse.py
+++ ifelse.py
@@ -1,2 +1,10 @@
 length  = int(input("Enter the length :"))
 breadth = int(input("Enter thr breadth :"))
+
+if length == breadth:
+    print("It is a Square")
+else:
+    print("It is not a Square")
+
+
+
