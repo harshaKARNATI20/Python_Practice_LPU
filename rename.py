bb9c5ddc21fb1501033013a18ec94a2f5ca8888b
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-10-31 23:53:49 +0530

    Rename

---
+++ rename.py
@@ -0,0 +1,8 @@
+# with open("Sample2.txt") as f:
+#     content=f.read()
+
+# with open("Renamed_by_python.txt","w") as f:
+#     f.write(content)
+
+import os
+os.rename("rename1.txt","files/rename.txt")
