b7f4d4680060b666e0fe3ff122ac8353e4c9fe29
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-08 22:04:19 +0530

    Exercise

---
+++ funcQs.py
@@ -0,0 +1,21 @@
+# # def name(name,age):
+# #     print(name)
+# #     print(age)
+
+# # name("Harsha Vardhan","20Years")
+
+# def num(*args):
+#     for i in args:
+#         print(i)
+
+# num(2,4,6,8)
+
+def name(firstname="Buddi", age=19):
+    print("My name is " + firstname,age)
+
+name("Harsha")
+name("Sravs",101)
+
+
+
+
