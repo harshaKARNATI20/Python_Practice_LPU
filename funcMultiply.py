b0ca2d2beb676f63b8bd54a2cf74aee8dc766bef
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-08 22:02:40 +0530

    Multiplication of Functions

---
+++ funcMultiply.py
@@ -0,0 +1,13 @@
+# def Multiply(a,b):
+#     into = a * b
+#     return into
+
+# x = Multiply(3,5)
+# print(x)
+
+def divide(a,b):
+    division = a // b
+    return division
+
+x = divide(150,5)
+print(x)
