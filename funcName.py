2d6d9d7ba1af6e75929531e217fdcf52de8a4cd0
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-08 22:03:44 +0530

    Prints Names

---
+++ funcName.py
@@ -0,0 +1,10 @@
+# def name(*myself):
+#     print(myself[4])
+
+# name("Harsha","vardhan","Karnati","20 Years","Resident of Narayanapuram")
+
+def name(name,*args):
+    print(name)
+    print(args)
+
+name("Harsha","Sri ram","Mehroj")
