6b4a6a327f4e2a7ba08bf1a7d247928e54bc30f0
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-13 21:44:05 +0530

    Que2 of Functions

---
+++ funcQs2.py
@@ -0,0 +1,30 @@
+# def count(listOfNum):
+#     even = 0
+#     odd = 0
+
+#     for i in listOfNum:
+#         if i%2==0:
+#             even += 1
+#         else:
+#             odd += 1
+#     return even,odd
+
+
+# listOfNum = [32, 21, 64, 100, 13]
+
+# even,odd = count(listOfNum)
+# print(even)
+# print(odd)
+
+
+# create a function that will diaplay the how many items in the list have names more than 5 characters
+
+# names = ["Atul","Shubham","Anurag","Rahul","Dev","Roy"]
+
+def count(NumOfLetters):
+    for i in NumOfLetters:
+        if i < 5:
+            print (i)
+
+
+
