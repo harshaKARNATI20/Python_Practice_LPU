27d915016feac11feac089aec8d4aca7e1f8df8d
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-20 18:15:49 +0530

    Second Loop

---
+++ listLoop2.py
@@ -0,0 +1,17 @@
+#list can print different types like strings,integers,floats etc..,
+#lists are ordered # can print duplicate values 
+# list is denoted by [] whereas tuple is denoted by ()
+colors = ["Red","Blue","Black","Red",22]
+cars = ["AUDI","MERC","LAMBO","FERRARI"]
+players = ("Dhoni","Messi","Ronaldo","Kohli")
+numbers = [1,4,6,2,10]
+cars.sort()
+# numbers.sort()
+numbers.sort(reverse=True)
+print(cars)
+print(numbers)
+
+# newList = [x.lower() for x in cars]
+# newList = [x for x in cars if x == "AUDI"]
+#     expression item  list    condition   
+# print(newList)
