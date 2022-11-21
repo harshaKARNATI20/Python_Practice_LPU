22db8c7705fbc96b0ae927f30b182cf5721277ce
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-21 22:19:47 +0530

    List 2nd Question

---
+++ listQue2.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5,2,6]
+# numbers.pop(1)
+# numbers.insert(1,200)
+
+# print(numbers)
+
+indx = numbers.index(2)
+numbers[indx] = 200
+print(numbers)
+# methods for lists in python (search on Google)
