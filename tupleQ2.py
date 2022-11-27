0d846a9011d1636fae04e88454fbfab6ff02c3dd
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-27 22:33:30 +0530

    Tuple Question 3

---
+++ tupleQ2.py
@@ -0,0 +1,24 @@
+# tuple1 = ("Harsha",)
+# print(tuple1)
+
+# tuple1 = ("apple","orange","pineapple")
+# list1 = list(tuple1)
+# list1.pop(2)
+# tuple1 = tuple(list1)
+# print(tuple1)
+
+# tuple1 = ("apple","orange","pineapple")
+# del tuple1
+# print(tuple1)
+# If we delete the tuple and print it,It shows error as the tuple is not defined because it is deleted
+
+# tuple1= (1,2,3,[6,7],4,5)
+# print(tuple1[3][0],tuple1[4])
+
+# tuple1 = (1,2,3,[6,7],4,5)
+# tuple1[3][0]=8
+# print(tuple1)
+# tuple1 = 
+tuple1 = (1,2,3,[6,7],4,5)
+tuple1[3][1]=9
+print(tuple1)
