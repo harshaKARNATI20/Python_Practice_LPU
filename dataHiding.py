75b1592afa3e01867bded8787a0b47421f42890c
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-24 13:23:14 +0530

    Data Hiding

---
+++ dataHiding.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,name,age):
+        self.__name = name
+        self.__age = age
+# mutators setters accessors
+    def getName(self):
+        return self.__name
+
+    def getAge(self):
+        return self.__age
+
+person1 = Person("ABC",22)
+# print(person1.__name)
+print(person1._Person__name)
+print(person1._Person__age)
+
+
+        
