528c71ca742835ae951bc594017ba932ba49eba4
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-24 13:22:47 +0530

    Intro to OOP's

---
+++ oopsIntro.py
@@ -0,0 +1,15 @@
+# Object Oriented Programming
+# Most Important
+# Classes are like a Blue Print for Objects
+# x = 5
+# y = "6"
+# print(type(y))
+class Laptop:
+    def config(self):
+        print("i7","1tb","16GB")
+laptop1 = Laptop()
+laptop2 = Laptop()
+
+# Laptop.config()
+laptop1.config()
+ #attributes Behaviour
