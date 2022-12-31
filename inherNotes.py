72417d96ab0962a99685465f8789b7b5cbcb2238
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-31 20:27:52 +0530

    Notes about Parent and Child Classes

---
+++ inherNotes.py
@@ -0,0 +1,27 @@
+
+class A:
+    def __init__(self):
+        print("This is init of method A")
+
+    def method1(self):
+        print("This is method 1")
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("This is init of method B")
+
+    def method2(self):
+        print("This is method  2")
+
+class C(B,A):
+    def __init__(self):
+        super().__init__()
+        print("This is init of method 3")
+    
+    def method3(self):
+        print("This is method 3")
+
+objC = C()
+
+# the left element is given more preference when there are 2 variables
